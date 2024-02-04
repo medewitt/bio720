aca42d65862e53a1953e8a7316f7f73205fb9a47
Michael DeWitt <me.dewitt.jr@gmail.com>
2024-02-04 15:50:18 -0500

    update this

--- scratch.R
+++ scratch.R
@@ -8,3 +8,21 @@ binom.test(6,8, p =.5, "greater")
 
 
 mean(rnorm(1000,8, 1 * sqrt(8))<0)
+
+library(pwr)
+
+o <- function(x){
+    samp <- rbinom(1, size = x, prob = .75)
+    samp
+}
+
+o(2)
+prop.test(o(5), 4, correct = FALSE)
+
+with(list(mu = 0.2, mu.alt = 0.75, alpha = 0.05, n = 8), (mean(sapply(rbinom(1e4, n, mu.alt), function(x) prop.test(x, n, mu, alternative = "greater")$p.value <= alpha))))
+
+pbinom(4,5,.7)
+
+mean(pbinom(rbinom(1000,10,.7),10,.7)>=.8)
+
+pbinom(4,5, .7)
